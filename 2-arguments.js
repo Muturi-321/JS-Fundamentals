972650824491de43c3cda06dec2780f7f9778211
Muturi-321 <130498496+Muturi-321@users.noreply.github.com>
2025-09-14 21:15:30 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,5 +2,11 @@ const { argv } = require('node:process');
 
 // print process.argv
 argv.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
+  if (val.length == 0){
+    console.log('No argument');
+  }else if (val.langth == 1){
+    console.log('Argument found');
+  }else{
+    console.log('Arguments found');
+  }
 });

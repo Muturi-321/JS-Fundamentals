938dcb3eddf57affbd084836828491745b96e4f4
Muturi-321 <130498496+Muturi-321@users.noreply.github.com>
2025-09-14 21:08:11 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,11 +3,10 @@ let myVar = [1];
 if (myVar.length == 0){
   console.log('No argument');
 }
+  
 else if (myVar.length == 1){
   console.log('Argument found');
-
 }
 else{
   console.log('Arguments found');
-
 }

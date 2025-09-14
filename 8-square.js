76d10f292cff984f1e85d66443ded0d636c9f1fe
Muturi-321 <130498496+Muturi-321@users.noreply.github.com>
2025-09-14 22:39:37 +0300

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -3,9 +3,11 @@ const size = Number(args[0]);
 
 if (Number.isInteger(size)){
   for (let i = 0; i < size; i++){
+    let row = '';
     for (let j=0; j < size; j++){
+      row += 'X';
     }
-    console.log('x')
+    console.log(row);
   }
 }else{
   console.log('Missing size');

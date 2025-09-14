af6b5ff4a970cc3b66cb5fc5131ffab4220db05b
Muturi-321 <130498496+Muturi-321@users.noreply.github.com>
2025-09-14 22:12:17 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,7 +1,9 @@
 const args = process.argv.slice(2);
 
-if (Number.isInteger(Number(args))){
-  console.log(args[0] * 'C is fun');
-}else{
-  console.log('Missing number of occurrences');
+for (i=0; i== args[0]; i++){
+  if (Number.isInteger(Number(args))){
+    console.log(args[0] * 'C is fun');
+  }else{
+    console.log('Missing number of occurrences');
+  }
 }

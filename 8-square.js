7edddfc3c3f9ce8f74c919f8f8b7d973b7a2b3ae
Muturi-321 <130498496+Muturi-321@users.noreply.github.com>
2025-09-14 22:29:34 +0300

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,1 +1,12 @@
+const args = process.argv.slice(2);
+const size = Number(args[0]);
 
+if (Number.isInteger(size)){
+  for (let i = 0; i <= size; i++){
+    for (let j=0; j <= size; j++){
+      console.log('x');
+    }
+  }
+}else{
+  console.log('Missing size');
+}

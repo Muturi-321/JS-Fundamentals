05e34b52f153295c6afab1dc13b09c013fe8c305
Muturi-321 <130498496+Muturi-321@users.noreply.github.com>
2025-09-14 21:57:01 +0300

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,6 +1,6 @@
 const args = process.argv.slice(2);
 
-if (Number.isInteger(Number(args)){
+if (Number.isInteger(Number(args))){
   console.log(`My number: ${args[0]}`);
 }else{
   console.log('Not a number');

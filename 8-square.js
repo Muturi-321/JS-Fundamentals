e8c8d1583b7ddf7e1243fd633dca414fa51adb6f
Muturi-321 <130498496+Muturi-321@users.noreply.github.com>
2025-09-14 22:33:07 +0300

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -2,9 +2,9 @@ const args = process.argv.slice(2);
 const size = Number(args[0]);
 
 if (Number.isInteger(size)){
-  for (let i = 0; i <= size; i++){
-    for (let j=0; j <= size; j++){
-      //console.log('x');
+  for (let i = 0; i < size; i++){
+    for (let j=0; j < size; j++){
+      console.log('j');
     }
     console.log('x')
   }
